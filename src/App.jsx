dd52409959742a45b6134d88d3254891639a0d0a
aswins2108@gmail.com <aswins2108@gmail.com>
2022-01-28 12:45:05 +0530

    made the react app and did the clean up. Setup done!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-700 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
