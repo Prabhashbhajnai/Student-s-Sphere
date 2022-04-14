a3cc650d8315d020b80d5ee0371a8b469e228f75
anurag819 <87742653+anurag819@users.noreply.github.com>
2022-04-14 18:05:18 +0530

    Individual Book added

--- client/src/Layout/Library/IndividualBook.Library.Layout.jsx
+++ client/src/Layout/Library/IndividualBook.Library.Layout.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 // components
 import Navbar from '../../components/Navbar';
+import IndividualBook from '../../components/Library/IndividualBook/IndividualBook';
 
 const IndividualBookLayout = (props) => {
     return (
         <>
             <Navbar />
             <div className="">
-                {props.children}
+                <IndividualBook 
+                    image={"https://edit.org/images/cat/book-covers-big-2019101610.jpg"} 
+                    title={"Book Title"}
+                    description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis eos id in blanditiis amet quo magni veritatis repellat praesentium expedita. Reprehenderit velit error provident asperiores voluptatibus ab numquam corporis architecto."}
+                    author={"Author"}
+                    authorAbout={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eveniet natus harum, voluptate iusto delectus impedit ex, ab debitis, ratione quasi animi accusamus culpa illum doloribus. Porro architecto consectetur aspernatur."}
+                    publisher={"Publisher"}
+                    date={"29-Nov-21"}
+                    isbn={123456789}
+                />
             </div>
         </>
     );
