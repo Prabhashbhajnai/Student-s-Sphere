1c49080a644aed680a1e6c3cb62fdf5fe79f5740
Prabhashbhajani <prabhashbhajani@gmail.com>
2022-04-15 23:00:37 +0530

    Individual page aligned

--- client/src/components/Library/IndividualBook/Buttons.jsx
+++ client/src/components/Library/IndividualBook/Buttons.jsx
@@ -6,9 +6,9 @@ const Buttons = (props) => {
         <>
             <button 
                 className={classnames(
-                    "flex items-center bg-white px-3 py-2 rounded-lg border hover:bg-gray-300 hover:text-black",
+                    "flex items-center px-3 py-2 rounded-lg border hover:bg-red-400 hover:text-black",
                     {
-                        "bg-Library-400": props.isActive,
+                        "bg-red-400": props.isActive,
                         "text-white": props.isActive,
                     }
                 )}
--- client/src/components/Library/IndividualBook/IndividualBook.jsx
+++ client/src/components/Library/IndividualBook/IndividualBook.jsx
@@ -38,16 +38,16 @@ const IndividualBook = (props) => {
             </div>
 
 
-            <div className='hidden w-full h-80 md:flex flex gap-3'>
+            <div className='hidden w-full h-80 md:flex gap-3'>
                 <div className='w-5/12 h-auto'>
                     <img
                         src={props.image}
                         alt="book"
-                        className='w-96 h-100'
+                        className='w-[30rem] h-[38rem]'
                     />
                 </div>
                 <div className='w-7/12'>
-                    <div className='flex items-center justify-between border-b-2 scrollbar-hide'>
+                    <div className='flex items-center justify-between border-b-2 border-grey-200 pb-2'>
                         <div>
                             <h1 className='text-4xl font-semibold'>{props.title}</h1>
                             <h6>(Author)</h6>
@@ -58,18 +58,20 @@ const IndividualBook = (props) => {
                         </div>
                     </div>
 
-                    <p className='text-lg'>{props.description}</p>
+                    <div className='overflow-auto h-[34rem] scrollbar-hide'>
+                        <p className='text-lg'>{props.description}</p>
 
-                    <div className='mt-5 text-lg'>
-                        <p><span className='text-gray-500'>Author: </span>{props.author}</p>
-                        <p><span className='text-gray-500'>Publisher: </span>{props.publisher}</p>
-                        <p><span className='text-gray-500'>Published date: </span>{props.date}</p>
-                        <p><span className='text-gray-500'>ISBN: </span>{props.isbn}</p>
-                    </div>
+                        <div className='mt-5 text-lg'>
+                            <p><span className='text-gray-500'>Author: </span>{props.author}</p>
+                            <p><span className='text-gray-500'>Publisher: </span>{props.publisher}</p>
+                            <p><span className='text-gray-500'>Published date: </span>{props.date}</p>
+                            <p><span className='text-gray-500'>ISBN: </span>{props.isbn}</p>
+                        </div>
 
-                    <div className='mt-5'>
-                        <h2 className='text-2xl font-semibold'>About {props.author}</h2>
-                        <p className='text-lg'>{props.authorAbout}</p>
+                        <div className='mt-5'>
+                            <h2 className='text-2xl font-semibold'>About {props.author}</h2>
+                            <p className='text-lg'>{props.authorAbout}</p>
+                        </div>
                     </div>
                 </div>
             </div>
