d70c203e8edcabcabe5fb76cfc491178be98ca8a
Prabhashbhajani <prabhashbhajani@gmail.com>
2022-04-17 00:01:48 +0530

    Book reducer added for Romance

--- client/src/Redux/Reducer/books/book.action.js
+++ client/src/Redux/Reducer/books/book.action.js
@@ -15,3 +15,16 @@ export const getFantasyFiction = () => async (dispatch) => {
         return dispatch({ type: "ERROR", payload: error });
     }
 };
+
+export const getRomance = () => async (dispatch) => {
+    try {
+        const bookList = await axios({
+            method: "GET",
+            url: `http://localhost:4000/books/?genre=romance`,
+        });
+
+        return dispatch({ type: GET_BOOKS, payload: bookList.data });
+    } catch (error) {
+        return dispatch({ type: "ERROR", payload: error });
+    }
+};
