369d106bf7c8231335d1bec1fde7b37656b78ec8
Prabhashbhajani <prabhashbhajani@gmail.com>
2022-04-17 00:18:03 +0530

    Added z-index to navbar and alligned book cards

--- client/src/components/Library/Books/BookCard.jsx
+++ client/src/components/Library/Books/BookCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BookCard = (props) => {
     return (
         <>
-            <div className=" bg-white shadow-lg rounded-xl w-24 h-80 md:w-56  transition duration-1000 hover:scale-105">
+            <div className=" bg-white shadow-lg px-1 rounded-xl w-24 h-80 md:w-56  transition duration-1000 hover:scale-105">
                 <div className="w-full h-32">
                     <img
                         src={props.photo} 
@@ -11,7 +11,7 @@ const BookCard = (props) => {
                         className="w-full h-56 rounded-t-md px-6 pt-4 pb-2"
                     />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col px-1">
                     <h3 className="text-md text-center font-bold mt-24">{props.title}</h3>
                     <div className="items-center">
                         <p className='text-center'>
